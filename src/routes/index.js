ea828d00f29cb1d36c110d9d8da595e6862b11ba
datlt <datlt132@gmail.com>
2023-08-07 23:32:26 +0700

    comment chat

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,5 @@ const router = require('express').Router()
 router.use("/api", require("./api/auth"))
 router.use("/api", require("./api/post"))
 router.use("/api", require("./api/rating"))
-router.use("/api", require("./api/chat"))
+// router.use("/api", require("./api/chat"))
 module.exports = router;
