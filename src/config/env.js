6425298787ed940e2679ec2a303dcc244701c51f
datlt <datlt132@gmail.com>
2023-08-11 03:14:07 +0700

    update JWTSecret

--- src/config/env.js
+++ src/config/env.js
@@ -3,7 +3,7 @@ const data = {};
 const loadEnv = () => {
     data.MongoDBUrl = process.env.MONGODB_URL;
     data.Port = process.env.PORT;
-    data.JwtSecret = process.env.JWT_SECRET;
+    data.JWTSecret = process.env.JWT_SECRET;
 }
 
 module.exports = {
